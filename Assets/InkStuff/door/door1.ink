bfcf4173b792bb762be87ec47f947abfa3fc0135
texasheadache <jranck333@gmail.com>
2020-11-19 03:33:26 -0600

    idle animations

--- Assets/InkStuff/door/door1.ink
+++ Assets/InkStuff/door/door1.ink
@@ -1,6 +1,6 @@
 i don't wanna leave. 
 i want to disappear 
 into the corner of the room. 
-or to make a time machine
+or make a time machine
 to save my past self. 
 my mom will know i'm high.
--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -8,11 +8,13 @@ public class Movement : MonoBehaviour
     private Rigidbody2D myRigidbody;
     public float speed;
     public Vector3 change;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,11 @@ public class Movement : MonoBehaviour
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
         UpdateAnimationAndMove();
+        if (change != Vector3.zero)
+            {
+            anim.SetFloat("movex", change.x);
+            anim.SetFloat("movey", change.y);
+             }
     }
 
 
