4c868e69f03f3f33e54e7770c11aaccb512d198d
texasheadache <jranck333@gmail.com>
2020-11-28 01:09:49 -0600

    added simple pause menu w/ resume & quit

---
+++ Assets/Scripts/Functional/Pause.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] public GameObject pausePanel;
+    //[SerializeField] public GameObject optionsPanel;
+    [SerializeField] AudioSource playMusic;
+    [SerializeField] AudioSource playPauseMusic;
+   // [SerializeField] AudioSource playCompMusic;
+    private GameObject player;
+
+    void Start()
+    {
+        //pausePanel.SetActive(false);
+        player = GameObject.Find("Player");
+        Debug.Log("FoundIt");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("first");
+            if (!pausePanel.activeInHierarchy)
+            {
+                Debug.Log("second");
+                PauseGame();
+            }
+
+            else if (pausePanel.activeInHierarchy)
+            {
+                UnPauseGame();
+                Debug.Log("third");
+                playMusic.mute = false;
+                player.GetComponent<Movement>().unFreeze();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        if (!pausePanel.activeInHierarchy)
+        {
+            pausePanel.SetActive(true);
+        }
+        playMusic.mute = true;
+        playPauseMusic.mute = false;
+        Debug.Log("Unmuted");
+        player.GetComponent<Movement>().freeze();
+    }
+
+    public void UnPauseGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+      //  optionsPanel.SetActive(false);
+        playMusic.mute = false;
+        playPauseMusic.mute = true;
+        player.GetComponent<Movement>().unFreeze();
+    }
+
+
+}
---
+++ Assets/Scripts/Functional/Quit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour
+{
+    public Pause paused;
+    private GameObject player;
+
+    public void Start()
+    {
+        player = GameObject.Find("Player");
+
+    }
+
+    public void OnButtonPress()
+    {
+         Application.Quit();
+  
+
+    }
+
+}
